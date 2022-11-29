783d212b53c5bea4c3ed5cd4b6506300ae8f3c45
aharshbe <1@97104.de>
2022-11-29 15:53:30 -0800

    finishing up the functions section and moving to control flow via a new project folder 'branches'

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,6 +6,19 @@ fn main() {
     another_function(5);
     print_labeled_measurement(5, 'h');
 
+    let y = {
+        let x = 3;
+        x + 1 // becomes an expression without a semicolon, otherwise statement
+    };
+
+    println!("The value of y is: {:?}", y);
+
+
+    let r = five(10);
+    println!("the return value of the function, 'five' is: {r}");
+
+    let ad = add_two(1, 2);
+    println!("the return value of the function, 'add_two' is: {ad}");
 }
 
 fn first_another_function(){
@@ -21,4 +34,14 @@ fn another_function (x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+// Functions that return values
+
+fn five(x: i32) -> i32 {
+    x + 5
+}
+
+fn add_two(x: i32, y: i32) -> i32 {
+    x + y
 }
